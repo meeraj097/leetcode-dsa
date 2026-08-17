d477ad947c781f172287045c1d8c01b04ea92e3e
Kakarla Meeraj <152650526+meeraj097@users.noreply.github.com>
2026-08-17 22:09:29 +0530

    Time: 0 ms (100%), Space: 44.8 MB (16.23%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -2,7 +2,9 @@ class Solution {
     public void nextPermutation(int[] nums) {
 
         int n= nums.length;
-        int ind =-1;
+
+        int ind=-1;
+
         for(int i=n-2;i>=0;i--)
         {
             if(nums[i]<nums[i+1])
@@ -11,15 +13,17 @@ class Solution {
                 break;
             }
         }
+
         if(ind == -1)
         {
             reverse(nums,0,n-1);
             return;
         }
 
-        for(int i=n-1;i>ind;i--)
+        for(int i= n-1;i>ind;i--)
         {
-            if(nums[i]>nums[ind])
+
+            if(nums[i]> nums[ind])
             {
                 int temp = nums[i];
                 nums[i] = nums[ind];
@@ -27,19 +31,23 @@ class Solution {
                 break;
             }
         }
-       reverse(nums,ind+1,n-1);
+        reverse(nums, ind+1, n-1);
     }
 
-    public void reverse(int[] nums, int l, int r)
+    public void  reverse(int[] nums, int l, int r)
     {
+
         while(l<r)
         {
-             int temp = nums[l];
-                nums[l] = nums[r];
-                nums[r] = temp;
+            int temp = nums[l];
 
-                l++;
-                r--;
+            nums[l] = nums[r];
+
+            nums[r] = temp;
+
+            l++;
+
+            r--;
         }
     }
 }
